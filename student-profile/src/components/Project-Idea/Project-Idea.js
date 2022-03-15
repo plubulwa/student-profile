3f6f82e64030944e6ae63ea682d96c59ca1015a6
Peter L <61480168+plubulwa@users.noreply.github.com>
2022-03-15 15:09:02 +1100

    Project Structure added

--- student-profile/src/components/Project-Idea/Project-Idea.js
+++ student-profile/src/components/Project-Idea/Project-Idea.js
@@ -7,6 +7,10 @@ export default function ProjectIdea() {
             <p><i>where I try to solve a mundane problem</i></p>
             <br />
             <p>#content</p>
+            <br />
+            <hr />
+            <h2>Overview</h2>
+            <p><i>100 words</i></p>
             <p>At this stage - the ideal project is a modular project, designed to solve a small problem... Ths first problem I can think about would be Marco Polo - this is
                 A game that already exists ( pokemon go) but the aim is not to make a game - it is to practice iOS and/ or Android app development  using
                 React Native, Xamarin, or come other code once, deploy everywhere native platfomr
@@ -16,9 +20,29 @@ export default function ProjectIdea() {
                 Simple rewards system for catching someone - its a never-enging game but you can opt out easily ( of course)...
             </p>
             <br />
+            <h2>Motivation</h2>
+            <p><i>100 words</i></p>
             <p>The motiviaion is to start to create apps in native/ xamarin that utilise some of the common API's that can be used for a mobile app system, and to try and get all of
                 of these things to communicate together in a seamless way.
             </p>
+            <br />
+            <h2>Description</h2>
+            <p><i>100 words</i></p>
+            <p>Here is the overview of the project</p>
+            <br />
+            <h2>Tools and Technologies</h2>
+            <p><i>100 words</i></p>
+            <p>Here is the overview of the project</p>
+            <br />
+            <h2>Skills Required</h2>
+            <p><i>100 words</i></p>
+            <p>Here is the overview of the project</p>
+            <br />
+            <h2>Outcome</h2>
+            <p><i>100 words</i></p>
+            <p>What is the outcome</p>
+            <br />
+
 
         </div >
     );
